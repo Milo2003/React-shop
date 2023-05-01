df38b597cf70f2dcc3e33fffc442cb6d66cfc804
Milo2003 <milonorenalo@gmail.com>
2023-05-01 16:19:16 -0500

    [Fix] Cambio a hashrouter

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 import '../style/global.scss';
 import { CreatePassword } from '../pages/CreatePassword';
@@ -17,7 +17,7 @@ const App = () => {
     const initialState = useInitialState();
     return (
         <AppContext.Provider value={initialState}>
-            <BrowserRouter basename='/React-shop'>
+            <HashRouter basename='/React-shop'>
                 <Layout>
                     <Routes>
                         <Route path='/home' element={<Home />} />
@@ -30,7 +30,7 @@ const App = () => {
                         <Route path='*' element={<NotFound />} />
                     </Routes>
                 </Layout>
-            </BrowserRouter>
+            </HashRouter>
         </AppContext.Provider>
     );
 }
